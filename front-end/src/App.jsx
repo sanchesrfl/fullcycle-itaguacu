03b447de74e47f575972fda6d94c6ab46ad04284
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2023-08-25 18:48:57 -0300

    feat: first step chat room

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="vh-100 d-flex justify-content-center align-items-center">
+      <div className="container chat-container">
+        <div className="row">
+          <div className="col-md-8 offset-md-2 chat-box">
+            <div className="title text-center">
+              <h5>NOME DO GRUPO</h5>{" "}
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div class="input-group mb-3">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Digite sua mensagem"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+            <div className="input-group-append">
+              <button className="btn btn-outline-success" type="button">
+                Enviar
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
